449c3386767bba7fb73244ae9f1f376123773b12
ryantillis <ryantillis@me.com>
2016-11-17 17:47:52 -0500

    JS click fix

--- js/main.js
+++ js/main.js
@@ -5,12 +5,6 @@ $(document).ready(function () {
   });
 });
 
-// Pause the video when the modal is closed
-$(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
-    // Remove the src so the player itself gets removed, as this is the only
-    // reliable way to ensure the video stops playing in IE
-    $("#trailer-video-container").empty();
-});
 // Start playing the video whenever the trailer modal is opened
 $(document).on('click', '.movie-tile', function (event) {
     var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
@@ -22,3 +16,11 @@ $(document).on('click', '.movie-tile', function (event) {
       'frameborder': 0
     }));
 });
+
+// Pause the video when the modal is closed
+$(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+    // Remove the src so the player itself gets removed, as this is the only
+    // reliable way to ensure the video stops playing in IE
+    $("#trailer-video-container").empty();
+});
+
