ce4001435c3a71b17f316dd9c638851955c16476
ryantillis <ryantillis@me.com>
2016-11-17 18:05:04 -0500

    readded self

--- media.py
+++ media.py
@@ -8,7 +8,7 @@ class Movie():
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
         
-    def show_trailer():
+    def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
         
         
