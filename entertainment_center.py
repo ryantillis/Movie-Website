6303e9ca566105826fdb787c863c7af53fe45411
ryantillis <ryantillis@me.com>
2016-11-17 15:19:51 -0500

    Added Fresh tomatoes and movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,3 +6,42 @@ Created on Thu Nov 17 14:21:39 2016
 @author: Ryan
 """
 
+import media
+
+interstellar = media.Movie("Interstellar", 
+                           "A brilliant NASA physicist is working on plans to save mankind by transporting Earth's population to a new home via a wormhole.",
+                           "http://t1.gstatic.com/images?q=tbn:ANd9GcRf61mker2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB",
+                           "https://www.youtube.com/watch?v=sRLc9OlrZZw")
+
+pan = media.Movie("Pan's Labyrinth",
+                  "Ofelia encounters the faun Pan, who tells her that she is a legendary lost princess and must complete three dangerous tasks in order to claim immortality.",
+                  "http://www.gstatic.com/tv/thumb/movieposters/162074/p162074_p_v8_ab.jpg",
+                  "https://www.youtube.com/watch?v=EqYiSlkvRuw")
+
+shadows = media.Movie("What We Do in the Shadows",
+                          "Vampire housemates try to cope with the complexities of modern life and show a newly turned hipster some of the perks of being undead.",
+                          "http://t1.gstatic.com/images?q=tbn:ANd9GcTb1oWD0EwTDmLFAu0i1IIgAQLSCITuuVeExzzWpIE14xiVm1B7",
+                          "https://www.youtube.com/watch?v=Cv568AzZ-i8")
+
+moonrise = media.Movie("Moonrise Kingdom",
+                       "The year is 1965, and the residents of New Penzance, an island off the coast of New England, inhabit a community that seems untouched by some of the bad things going on in the rest of the world. ",
+                       "http://t1.gstatic.com/images?q=tbn:ANd9GcTc9Qfqux_R9ycASZUOHD0R-1wNYeGYg-0Y9ICVmKOKekAGhplc",
+                       "https://www.youtube.com/watch?v=_eOI3AamSm8")
+
+empire = media.Movie("Star Wars: The Empire Strikes Back",
+                     "Luke Skywalker, Han Solo, Princess Leia and Chewbacca face attack by the Imperial forces and its AT-AT walkers on the ice planet Hoth.",
+                     "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2015/07/ESB-Special-Edeition.jpg",
+                     "https://www.youtube.com/watch?v=xESiohGGP7g")
+
+twotowers = media.Movie("The Lord of the Rings: The Two Towers",
+                        "Follows the continuing quest of Frodo (Elijah Wood) and the Fellowship to destroy the One Ring.",
+                        "http://www.gstatic.com/tv/thumb/movieposters/30793/p30793_p_v8_am.jpg",
+                        "https://www.youtube.com/watch?v=2dlRvAjU_RI")
+
+pan.show_trailer()
+
+#blackmirror =
+        #https://www.youtube.com/watch?v=THP9YQ3WT1k
+        
+#strangerthings =        
+        #https://www.youtube.com/watch?v=XWxyRG_tckY
--- media.py
+++ media.py
@@ -5,11 +5,19 @@ Created on Thu Nov 17 14:18:22 2016
 
 @author: Ryan
 """
+import webbrowser
 
 class Movie():
-    def _init_(self,movie_title, movie_storyline, poster_image,trailer_youtube:
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
-        sefl.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer)
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+        
+        
+#class Tv():
+#    def _init_(self,tv_title,tv_storyline,poster_image,trailor_youtube):
         
