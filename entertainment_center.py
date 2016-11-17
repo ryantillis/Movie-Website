7e009ee9ff74b2744e4c8992bcee37e24eb0387e
ryantillis <ryantillis@me.com>
2016-11-17 17:15:29 -0500

    Reverted to single file

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 17 14:21:39 2016
-
-@author: Ryan
-"""
 import fresh_tomatoes
 import media
 
--- js/main.js
+++ js/main.js
@@ -1,18 +1,10 @@
-// Animate in the movies when the page loads
-$(document).ready(function () {
-  $('.movie-tile').hide().first().show("fast", function showNext() {
-    $(this).next("div").show("fast", showNext);
-  });
-});
-
-// Pause the video on close
+// Pause the video when the modal is closed
 $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
     // Remove the src so the player itself gets removed, as this is the only
     // reliable way to ensure the video stops playing in IE
     $("#trailer-video-container").empty();
 });
-  
-// Start playing on open
+// Start playing the video whenever the trailer modal is opened
 $(document).on('click', '.movie-tile', function (event) {
     var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
     var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
@@ -22,4 +14,10 @@ $(document).on('click', '.movie-tile', function (event) {
       'src': sourceUrl,
       'frameborder': 0
     }));
+});
+// Animate in the movies when the page loads
+$(document).ready(function () {
+  $('.movie-tile').hide().first().show("fast", function showNext() {
+    $(this).next("div").show("fast", showNext);
+  });
 });
