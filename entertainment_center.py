eba423f41ddc36773550e9543bbc0b448a9a74b6
ryantillis <ryantillis@me.com>
2016-11-17 14:25:23 -0500

    Initial commit class structures

---
+++ entertainment_center.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 14:21:39 2016
+
+@author: Ryan
+"""
+
---
+++ media.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 14:18:22 2016
+
+@author: Ryan
+"""
+
+class Movie():
+    def _init_(self,movie_title, movie_storyline, poster_image,trailer_youtube:
+        self.title = movie_title
+        self.storyline = movie_storyline
+        sefl.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer)
+        
